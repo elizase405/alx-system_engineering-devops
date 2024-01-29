b12651d944a4de4984931ae9a8cab421ccec44e0
Elizabeth <elizase405@gmail.com>
2024-01-29 07:18:22 -0800

    feat: task 4

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+# setup ssh config file to allow for connecting to server without password
+
+file {'/etc/ssh/ssh_config':
+ensure    => present,
+content   => "PasswordAuthentication no\nIdentityFile ~/.ssh/school",
+}
