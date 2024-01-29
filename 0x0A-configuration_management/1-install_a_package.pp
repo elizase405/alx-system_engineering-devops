d6000541e08ac2a3181ababda5fd2fee70b6393b
Elizabeth <elizase405@gmail.com>
2024-01-28 22:24:13 -0800

    refactor: task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,7 @@
+#!/usr/bin/pup
 # install flask version 2.1.0
 
-package {'python3-pip':
-ensure   => installed,
-}
-
-exec {'install-flask':
-command => '/usr/bin/pip3 install flask==2.1.0',
-creates => '/usr/local/lib/python3.8/dist-packages/flask',
+package {'flask':
+provider => 'pip3',
+ensure   => '2.1.0',
 }
