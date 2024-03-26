03fca3df2d8a0ae10273d95acb7390f29bdbd4dd
Elizabeth <elizase405@gmail.com>
2024-03-25 20:56:03 -0700

    feat: Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Use https://jsonplaceholder.typicode.com/ API to get data
+about a certain employee
+"""
+
+import requests
+import json
+
+if __name__ == "__main__":
+    uri = "https://jsonplaceholder.typicode.com"
+    users_info = {}
+    file_name = "todo_all_employees.json"
+
+    users = requests.get(f"{uri}/users").json()
+    todos = requests.get(f"{uri}/todos").json()
+    for user in users:
+        user_id = user.get("id")
+        user_info = []
+        for todo in todos:
+            userId = todo.get("userId")
+            user_data = {}
+            if user_id == userId:
+                user_data["username"] = user.get("username")
+                user_data["task"] = todo.get("title")
+                user_data["completed"] = todo.get("completed")
+            if user_data != {}:
+                user_info.append(user_data)
+        users_info[user_id] = user_info    
+
+    with open(file_name, "w") as json_file:
+        json.dump(users_info, json_file)
