4f427b1a1194ae2ae3e37636206049937efc94ba
Elizabeth <elizase405@gmail.com>
2024-03-28 06:22:09 -0700

    feat: task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Use https://jsonplaceholder.typicode.com/ API to get data
+about a certain employee
+"""
+
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    uri = "https://jsonplaceholder.typicode.com"
+    emp_id = int(sys.argv[1])
+    csv_file_path = f"{emp_id}.csv"
+    datas = []
+
+    user = requests.get(f"{uri}/users/{emp_id}").json()
+    emp_name = user.get("username")
+
+    todos = requests.get(f"{uri}/todos").json()
+    for todo in todos:
+        data = {}
+        userId = todo.get("userId")
+        if userId == emp_id:
+            data["id"] = userId
+            data["name"] = emp_name
+            data["done"] = todo.get("completed")
+            data["title"] = todo.get("title")
+
+        if data != {}:
+            datas.append(data)
+
+    with open(csv_file_path, mode="w", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=datas[0].keys())
+
+        for data in datas:
+            writer.writerow(data)
