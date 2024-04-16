152dfaccd1a247871eef91607a1837a76ea3a718
elizase405 <elizase405@gmail.com>
2024-04-16 08:40:02 +0000

    Add comment

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,5 @@
+# Fix 500 error on an Apache / Wordpress Server
+
 file { '/var/www/html/wp-settings.php':
   ensure  => present,
   content => inline_template('<%= File.read("/var/www/html/wp-settings.php").gsub(/\.phpp/, ".php") %>'),
