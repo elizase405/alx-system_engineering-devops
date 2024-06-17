50f8f5ddebf4cc8188224b6b87c424739cb3b52d
Elizabeth <elizase405@gmail.com>
2024-06-17 06:09:35 +0000

    feat: use different method

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,76 +1,13 @@
-ure file descriptor limits for the nginx user
-file { '/etc/security/limits.d/nginx.conf':
-  ensure  => file,
-  content => "nginx soft nofile 65536\nnginx hard nofile 65536\n",
+# Increases the amount of traffic an Nginx server can handle.
+
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
-
-# Ensure pam_limits.so is included in common-session
-augeas { 'pam_limits_common-session':
-  context => '/files/etc/pam.d/common-session',
-  changes => 'ins 01 session before last; set 01/session/type required; set 01/session/module pam_limits.so',
-  onlyif  => 'match session[. = "required" and module = "pam_limits.so"] size == 0',
-  require => File['/etc/security/limits.d/nginx.conf'],
-}
-
-# Ensure pam_limits.so is included in common-session-noninteractive
-augeas { 'pam_limits_common-session-noninteractive':
-  context => '/files/etc/pam.d/common-session-noninteractive',
-  changes => 'ins 01 session before last; set 01/session/type required; set 01/session/module pam_limits.so',
-  onlyif  => 'match session[. = "required" and module = "pam_limits.so"] size == 0',
-  require => File['/etc/security/limits.d/nginx.conf'],
-}
-
-# Ensure the nginx configuration file has the correct content
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  content => "
-user  nginx;
-worker_processes  auto;
-worker_rlimit_nofile 65536;
-
-events {
-    worker_connections 1024;
-}
-
-http {
-    include       /etc/nginx/mime.types;
-    default_type  application/octet-stream;
-
-    log_format  main  '\$remote_addr - \$remote_user [\$time_local] \"\$request\" '
-                      '\$status \$body_bytes_sent \"\$http_referer\" '
-                      '\"\$http_user_agent\" \"\$http_x_forwarded_for\"';
-
-    access_log  /var/log/nginx/access.log  main;
-
-    sendfile        on;
-    #tcp_nopush     on;
-
-    keepalive_timeout  65;
-
-    #gzip  on;
-
-    include /etc/nginx/conf.d/*.conf;
-}
-",
-  notify  => Exec['nginx_reload'],
-}
-
-# Define the exec resource to reload nginx
-exec { 'nginx_reload':
-  command     => 'nginx -s reload',
-  refreshonly => true,
-}
-
-# Ensure the nginx service is running
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/nginx.conf'],
-  require   => Package['nginx'],
-}
-
-# Ensure nginx is installed
-package { 'nginx':
-  ensure => installed,
-}
-
