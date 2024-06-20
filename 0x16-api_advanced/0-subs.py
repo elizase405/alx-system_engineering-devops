3094b299def43a0a4b03a70f515355a2e63f5518
Elizabeth <elizase405@gmail.com>
2024-06-20 11:12:32 -0700

    testing

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,30 @@
-#!/usr/bin/python3
 
-"""This is the subs module.
-This module defines 1 function: def number_of_subscribers(subreddit)
+#!/usr/bin/python3
+"""Queries the `Reddit API`, returns the number of subscribers
+    of a subreddit
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    header = {"User-Agent": "Mozilla/5.0"}
+
+    data = requests.get(url, headers=header, allow_redirects=False)
+
+    if data.status_code == 200:
+        subscribers = data.json().get("data").get("subscribers")
+        return subscribers
+    return 0
+"""This is the subs module.
+This module defines 1 function: def number_of_subscribers(subreddit)
+"""
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    
         queries the Reddit API.
         Args:
             - subreddit (str) : The subreddit to be queried
@@ -19,15 +36,5 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = { "User-Agent" : "MyApp/1.0" }
     res = requests.get(url, headers=headers,  allow_redirects=False) #.json()"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    header = {"User-Agent": "Mozilla/5.0"}
-
-    data = requests.get(url, headers=header, allow_redirects=False)
-    print(data)
-    if data.status_code == 200:
-        subscribers = data.json().get("data").get("subscribers")
-        return subscribers
-    return 0
-    return 1
 #res.data.subscribers
 
